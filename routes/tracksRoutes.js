ffcf92ba3862b3ea91fdb6b8d9b3edeb37878247
yakiv-k <yakiv.y.k@gmail.com>
2023-01-25 23:36:08 -0500

    Updated knex configuration

--- routes/tracksRoutes.js
+++ routes/tracksRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const knex = require("knex")(require("../knexfile"));
+// const knex = require("knex")(require("../knexfile").development);
 const { S3Client } = require("@aws-sdk/client-s3");
 const multer = require("multer");
 const multerS3 = require("multer-s3");
