bbe6caab5a9fa08c1b5539aec90a4bd50460cdc7
yakiv-k <yakiv.y.k@gmail.com>
2023-02-01 14:14:04 -0500

    BUGS

--- knexfile.js
+++ knexfile.js
@@ -11,15 +11,8 @@ const connections = {
     },
   },
   production: {
-    client: "mysql",
+    client: "mysql2",
     connection: process.env.RAILWAYDB_URL,
-    // connection: {
-    //   host: "kollab-db.cscqquyuklfs.ca-central-1.rds.amazonaws.com",
-    //   port: "3306",
-    //   user: "admin",
-    //   password: "kollabkollab",
-    //   database: "kollabDB",
-    // },
     migrations: {
       directory: "./migrations",
     },
