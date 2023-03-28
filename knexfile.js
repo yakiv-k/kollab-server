fe6789cc71661b9d7a4b13dceb2fbd99b9710640
yakiv-k <yakiv.y.k@gmail.com>
2023-03-27 23:09:08 -0400

    Update mysql

--- knexfile.js
+++ knexfile.js
@@ -11,7 +11,7 @@ const connections = {
     },
   },
   production: {
-    client: "mysql2",
+    client: "mysql",
     connection: process.env.RAILWAYDB_URL,
     migrations: {
       directory: "./migrations",
